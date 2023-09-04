b276c2aa3fa07361a5584bba6a3531271ac82ba8
malit mohamut <60866464+mawlid1431@users.noreply.github.com>
2023-09-04 14:36:54 +0300

    challenge.js

--- challenge.js
+++ challenge.js
@@ -1,2 +1,17 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+
+
+                                                                                         function calculatePoints(wins, draws, losses) {
+  const winPoints = wins * 3;
+  const drawPoints = draws * 1;
+  const totalPoints = winPoints + drawPoints;
+  
+  console.log("Total points: " + totalPoints);
+  return totalPoints;
+}
+
+// Example usage:
+const totalPoints = calculatePoints(5, 2, 3); // Output: 17
+
